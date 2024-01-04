e55c801f704c5cfe3de92be0d7b511a5ecc78ccd
aradwanski <Artur.Radw@wp.pl>
2024-01-04 15:04:43 +0100

    Camera (startPreview wywala)

--- java/com/example/paragon/MainActivity.java
+++ java/com/example/paragon/MainActivity.java
@@ -2,14 +2,21 @@ package com.example.paragon;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.UseCaseGroup;
 import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -18,7 +25,10 @@ import android.widget.Button;
 import com.example.paragon.databinding.ActivityMainBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.ExecutionException;
+import java.util.function.IntPredicate;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
@@ -34,18 +44,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(view);
 
         binding.scanButton.setOnClickListener(this::handleScanClick);
+        binding.loadButton.setOnClickListener(this::handleLoadClick);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+
+        IntPredicate denied = (x) -> {return x==PackageManager.PERMISSION_DENIED;};
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(Arrays.stream(grantResults).anyMatch(denied)){
+            Log.d("logpermissions", "onRequestPermissionsResult: works");
+        }
+        else if(requestCode == 1){
+            this.startNewActivity(TakePicActivity.class);
+        }
+        else if(requestCode == 2){
+            this.startNewActivity(LoadPicActivity.class);
+        }
     }
 
     private void handleScanClick(View e) {
-//        ImageCapture imageCapture =
-//                new ImageCapture.Builder()
-//                        .setTargetRotation(binding.getRoot().getDisplay().getRotation())
-//                        .build();
-//
-//        this.cameraProvider.bindToLifecycle(this, CameraSelector.DEFAULT_BACK_CAMERA, imageCapture);
-        Log.d("test", "handleScanClick: ");
+
+
+        if(!Helpers.checkCameraPermission(this)){
+            String[] permissions = new String[1];
+            permissions[0] = (Manifest.permission.CAMERA);
+            ActivityCompat.requestPermissions(this, permissions, 1);
+        }
+        else {
+            this.startNewActivity(TakePicActivity.class);
+        }
+
 
     }
+    private void handleLoadClick(View e) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if(!Helpers.checkFilesPermission(this)) {
+                String[] permissions = new String[1];
+                permissions[0] = (Manifest.permission.READ_MEDIA_IMAGES);
+                requestPermissions(permissions, 2);
+            }
+            else {
+                this.startNewActivity(LoadPicActivity.class);
+            }
+        }
+        else {
+            this.startNewActivity(LoadPicActivity.class);
+        }
+    }
+    private void startNewActivity(Class c) {
+        Intent i = new Intent(this, c);
+        i.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+        startActivity(i);
+    }
+
+
+
 
 
 
--- java/com/example/paragon/TakePicActivity.java
+++ java/com/example/paragon/TakePicActivity.java
@@ -1,41 +1,138 @@
 package com.example.paragon;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.internal.CameraUseCaseAdapter;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.view.Surface;
 import android.view.View;
 
 import com.example.paragon.databinding.ActivityTakePicBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.concurrent.ExecutionException;
 
 public class TakePicActivity extends AppCompatActivity {
     private ActivityTakePicBinding binding;
-    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
-    private ProcessCameraProvider cameraProvider;
+    private CameraDevice mCameraDevice;
+    private HandlerThread mBackgroundHandlerThread;
+    private Handler mBackgroundHandler;
+    private CaptureRequest.Builder mCaptureRequestBuilder;
+    private CameraDevice.StateCallback mCameraDevicaStateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            mCameraDevice = camera;
+            Log.d("cameraId", "onOpened: ");
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+            camera.close();
+            mCameraDevice = null;
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+            camera.close();
+            mCameraDevice = null;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityTakePicBinding.inflate(getLayoutInflater());
-        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-
+        //cameraProviderFuture = ProcessCameraProvider.getInstance(this);
         View view = binding.getRoot();
         setContentView(view);
+        startBackgroundThread();
+        setUpCamera(binding.txtView.getWidth(), binding.txtView.getHeight());
+        startPreview();
 
+    }
+    @SuppressLint("MissingPermission")
+    private void setUpCamera(int width, int height)  {
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        try{
+            for(String cameraId: cameraManager.getCameraIdList()){
+                CameraCharacteristics cameraCharacteristics = cameraManager.getCameraCharacteristics(cameraId);
+                if(cameraCharacteristics.get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_BACK)
+                {
+                    cameraManager.openCamera(cameraId, mCameraDevicaStateCallback, mBackgroundHandler);
+                }
+            }
+        }
+        catch(CameraAccessException e){
 
-        cameraProviderFuture.addListener(this::requestCameraProvider, ContextCompat.getMainExecutor(this));
+        }
     }
 
-    private void requestCameraProvider() {
+    private void closeCamera(){
+        if(mCameraDevice != null){
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+        //stopBackgroundThread();
+
+    }
+    private void startPreview() {
+        SurfaceTexture surfaceTexture = binding.txtView.getSurfaceTexture();
+        surfaceTexture.setDefaultBufferSize(binding.txtView.getWidth(), binding.txtView.getHeight());
+        Surface previewSurface = new Surface(surfaceTexture);
+
         try {
-            this.cameraProvider = cameraProviderFuture.get();
-        } catch (ExecutionException | InterruptedException e) {
-            // No errors need to be handled for this Future.
-            // This should never be reached.
+            mCaptureRequestBuilder = mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+            mCaptureRequestBuilder.addTarget(previewSurface);
+            mCameraDevice.createCaptureSession(Collections.singletonList(previewSurface), new CameraCaptureSession.StateCallback() {
+                @Override
+                public void onConfigured(@NonNull CameraCaptureSession session) {
+                    try {
+                        session.setRepeatingRequest(mCaptureRequestBuilder.build(),null, mBackgroundHandler );
+                    } catch (CameraAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                @Override
+                public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+                    Log.d("CameraId", "onConfigureFailed: ");
+                }
+            }, null);
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    private void startBackgroundThread() {
+        mBackgroundHandlerThread  = new HandlerThread("Camera2Image");
+        mBackgroundHandlerThread.start();
+        mBackgroundHandler = new Handler(mBackgroundHandlerThread.getLooper());
+    }
+    private void stopBackgroundThread() {
+        mBackgroundHandlerThread.quitSafely();
+        try {
+            mBackgroundHandlerThread.join();
+            mBackgroundHandlerThread = null;
+            mBackgroundHandler = null;
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
     }
 }
