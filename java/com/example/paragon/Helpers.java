1f87616b6569c0dfcad070d80ac556ac14bab5f8
aradwanski <Artur.Radw@wp.pl>
2024-02-08 14:40:23 +0100

    Camera (dziala preview, próbuje zwalczyc disortion)

--- java/com/example/paragon/Helpers.java
+++ java/com/example/paragon/Helpers.java
@@ -4,8 +4,11 @@ import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.util.Size;
 
 import androidx.annotation.RequiresApi;
+
+import java.util.Comparator;
 
 public class Helpers {
     public static boolean checkCameraPermission(Context Ctx) {
@@ -17,5 +20,13 @@ public class Helpers {
     public static boolean checkFilesPermission(Context Ctx) {
         String filePermission = Manifest.permission.READ_MEDIA_IMAGES;
         return Ctx.checkSelfPermission(filePermission) == PackageManager.PERMISSION_GRANTED;
+    }
+    public static class ComopareSizeByArea implements Comparator<Size> {
+
+        @Override
+        public int compare(Size o1, Size o2) {
+            return Long.signum((long) o1.getWidth() * o1.getWidth() /
+                    (long) o2.getWidth() * o2.getHeight());
+        }
     }
 }
--- java/com/example/paragon/TakePicActivity.java
+++ java/com/example/paragon/TakePicActivity.java
@@ -1,13 +1,19 @@
 package com.example.paragon;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.impl.utils.CompareSizesByArea;
 import androidx.camera.core.internal.CameraUseCaseAdapter;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.core.content.ContextCompat;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -15,26 +21,46 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.HandlerThread;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.Surface;
+import android.view.TextureView;
 import android.view.View;
 
 import com.example.paragon.databinding.ActivityTakePicBinding;
 import com.google.common.util.concurrent.ListenableFuture;
 
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class TakePicActivity extends AppCompatActivity {
     private ActivityTakePicBinding binding;
+    private static SparseIntArray ORIENTATIONS = new SparseIntArray();
+    static {
+        ORIENTATIONS.append(Surface.ROTATION_0, 0);
+        ORIENTATIONS.append(Surface.ROTATION_90, 90);
+        ORIENTATIONS.append(Surface.ROTATION_180, 180);
+        ORIENTATIONS.append(Surface.ROTATION_270, 270);
+    }
+    private Size mPreviewSize;
     private CameraDevice mCameraDevice;
     private HandlerThread mBackgroundHandlerThread;
     private Handler mBackgroundHandler;
     private CaptureRequest.Builder mCaptureRequestBuilder;
+    private ImageReader mReader;
     private CameraDevice.StateCallback mCameraDevicaStateCallback = new CameraDevice.StateCallback() {
         @Override
         public void onOpened(@NonNull CameraDevice camera) {
@@ -54,6 +80,42 @@ public class TakePicActivity extends AppCompatActivity {
             mCameraDevice = null;
         }
     };
+    private TextureView.SurfaceTextureListener txtViewStateCallback = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(@NonNull SurfaceTexture surface, int width, int height) {
+            startPreview();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(@NonNull SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(@NonNull SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(@NonNull SurfaceTexture surface) {
+
+        }
+    };
+    private static  int sensorToDeviceRotation(CameraCharacteristics cameraCharacteristics, int deviceOrientation){
+        int sensorOrientation = cameraCharacteristics.get(CameraCharacteristics.SENSOR_ORIENTATION);
+        deviceOrientation = ORIENTATIONS.get(deviceOrientation);
+        return (sensorOrientation + deviceOrientation + 360) % 360;
+    }
+
+    private ImageReader.OnImageAvailableListener mImageReaderStateCallback = reader -> {
+        Image image = reader.acquireLatestImage();
+        ByteBuffer buffer = image.getPlanes()[0].getBuffer();
+        byte[] bytes = new byte[buffer.capacity()];
+        buffer.get(bytes);
+        Bitmap bitmapImage = BitmapFactory.decodeByteArray(bytes, 0, bytes.length, null);
+        MediaStore.Images.Media.insertImage(getContentResolver(), bitmapImage, "paragon", "picture to get Text");
+        image.close();
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,9 +125,14 @@ public class TakePicActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
         startBackgroundThread();
+        int x = binding.txtView.getWidth();
+        int y = binding.txtView.getHeight();
         setUpCamera(binding.txtView.getWidth(), binding.txtView.getHeight());
-        startPreview();
-
+        if(binding.txtView.isAvailable())
+            startPreview();
+        else
+            binding.txtView.setSurfaceTextureListener(txtViewStateCallback);
+        binding.takePicBtn.setOnClickListener(this::handleButtonClick);
     }
     @SuppressLint("MissingPermission")
     private void setUpCamera(int width, int height)  {
@@ -77,6 +144,18 @@ public class TakePicActivity extends AppCompatActivity {
                 {
                     cameraManager.openCamera(cameraId, mCameraDevicaStateCallback, mBackgroundHandler);
                 }
+
+                StreamConfigurationMap map = cameraCharacteristics.get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP);
+                int deviceOrientation = getWindowManager().getDefaultDisplay().getOrientation();
+                int totalRotation = sensorToDeviceRotation(cameraCharacteristics, deviceOrientation);
+                boolean swapRotation = totalRotation == 90 || totalRotation == 270;
+                int rotatedWidth = width;
+                int rotatedHeight = height;
+                if(swapRotation){
+                    rotatedHeight = width;
+                    rotatedWidth = height;
+                }
+                mPreviewSize = chooseOptimalSize(map.getOutputSizes(SurfaceTexture.class), rotatedWidth, rotatedHeight);
             }
         }
         catch(CameraAccessException e){
@@ -93,8 +172,13 @@ public class TakePicActivity extends AppCompatActivity {
 
     }
     private void startPreview() {
+
+        int x = binding.txtView.getWidth();
+        int y = binding.txtView.getHeight();
+        mReader = ImageReader.newInstance(x, y, ImageFormat.JPEG, 1);
+        mReader.setOnImageAvailableListener(mImageReaderStateCallback, mBackgroundHandler);
         SurfaceTexture surfaceTexture = binding.txtView.getSurfaceTexture();
-        surfaceTexture.setDefaultBufferSize(binding.txtView.getWidth(), binding.txtView.getHeight());
+        surfaceTexture.setDefaultBufferSize(x, y);
         Surface previewSurface = new Surface(surfaceTexture);
 
         try {
@@ -133,6 +217,39 @@ public class TakePicActivity extends AppCompatActivity {
             mBackgroundHandler = null;
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
+        }
+    }
+    @SuppressLint("MissingPermission")//cant enter this activity without permission
+    private void handleButtonClick(View v){
+        try {
+            mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_STILL_CAPTURE);
+        } catch (CameraAccessException e) {
+            AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+                dialog.setMessage(R.string.alert_no_camera_permission);
+                dialog.setTitle(R.string.alert_no_camera_permission_title);
+                dialog.setPositiveButton(R.string.positive_button, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        return;
+                    }
+                });
+                AlertDialog alt = dialog.create();
+                alt.show();
+        }
+    }
+    private static Size chooseOptimalSize(Size[] choices, int width, int height){
+        List<Size> bigEnough = new ArrayList<Size>();
+        for(Size option : choices) {
+            if(option.getHeight() == option.getWidth() * height / width &&
+            option.getWidth() >= width && option.getHeight() >= height) {
+                bigEnough.add(option);
+            }
+        }
+        if(bigEnough.size() >= 0) {
+            Size min = Collections.min(bigEnough, new CompareSizesByArea());
+            return min;
+        } else {
+            return choices[0];
         }
     }
 }
